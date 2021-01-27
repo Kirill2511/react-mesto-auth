95a4a9add3e7ea0dde7e3a929a229c9f0f57eb7a
Kirill <kirill.kirill2@yandex.ru>
2021-01-28 01:38:59 +0500

    Deploy Fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -15,21 +15,21 @@ export default class Api {
 
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
+      headers: this.getHeaders(),
     }).then((res) => this._getResponseData(res));
   }
 
   getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       method: 'GET',
-      headers: this._headers,
+      headers: this.getHeaders(),
     }).then((res) => this._getResponseData(res));
   }
 
   postNewCard(data) {
     return fetch(`${this._baseUrl}/cards`, {
       method: 'POST',
-      headers: this._headers,
+      headers: this.getHeaders(),
       body: JSON.stringify(data),
     }).then((res) => this._getResponseData(res));
   }
@@ -37,7 +37,7 @@ export default class Api {
   setUserInfo(data) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
-      headers: this._headers,
+      headers: this.getHeaders(),
       body: JSON.stringify({
         name: data.name,
         about: data.about,
@@ -48,7 +48,7 @@ export default class Api {
   setUserAvatar(data) {
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
-      headers: this._headers,
+      headers: this.getHeaders(),
       body: JSON.stringify({
         avatar: data.avatar,
       }),
@@ -58,22 +58,30 @@ export default class Api {
   deleteCard(cardID) {
     return fetch(`${this._baseUrl}/cards/${cardID}`, {
       method: 'DELETE',
-      headers: this._headers,
+      headers: this.getHeaders(),
     }).then((res) => this._getResponseData(res));
   }
 
   likeCard(cardId) {
     return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
       method: 'PUT',
-      headers: this._headers,
+      headers: this.getHeaders(),
     }).then((res) => this._getResponseData(res));
   }
 
   dislikeCard(cardId) {
     return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
       method: 'DELETE',
-      headers: this._headers,
+      headers: this.getHeaders(),
     }).then((res) => this._getResponseData(res));
+  }
+
+  getHeaders() {
+    const token = localStorage.getItem('jwt');
+    return {
+      ...this._headers,
+      'Authorization': `Bearer ${token}`,
+    };
   }
 }
 
