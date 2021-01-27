05d5c91ecd5da10d51e0aef0e7831390fc0f933e
Kirill <kirill.kirill2@yandex.ru>
2021-01-28 00:10:47 +0500

    Deploy

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.kirill25111.students.nomoredomains.work/';
 
 const checkResponse = (res) => {
   if (res.ok) return res.json();
